493b59ef7e724c1bfec1a7589ade603dce79d00e
DrMuhammadAzmi <muhammad.bin.azmi.republic@gmail.com>
2026-04-21 22:46:57 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,4 @@
+const VOCAB_DATA = [
+  { char: '爸爸', py: 'bà ba', en: 'father', ms: 'ayah / bapa', topic: 'Topic 3', emoji: '👨', strokes: [['父','爸'], ['爸爸']], tip: '爸 = 8 strokes...', count: '16' },
+  // Add new topics here anytime!
+];
